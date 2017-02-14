f28f3efc1eb415d4a1bc75b4ec1b361573c1116b
Ryan Castner <castner.rr@gmail.com>
2017-02-14 12:42:44 -0500

    Bump version: 0.1.7 → 0.2.0

--- behaviors/__init__.py
+++ behaviors/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.7'
+__version__ = '0.2.0'
 
 default_app_config = 'behaviors.apps.BehaviorsConfig'
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     from distutils.core import setup
 
 
-version = '0.1.7'
+version = '0.2.0'
 
 
 if sys.argv[-1] == 'publish':
