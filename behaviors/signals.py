fe7c5bd7db55d7cf61b8f1f5c6cdf074fde32910
Elinaldo Nascimento <elinaldo.java@gmail.com>
2017-02-22 22:33:58 -0300

    Create signal to observer after save, if exist .pk add modified timezone.now()

--- behaviors/signals.py
+++ behaviors/signals.py
@@ -1,3 +1,12 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-__author__ = '@elinaldosoft'
+# -*- coding: utf-8
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils import timezone
+from .behaviors import Timestamped
+
+
+@receiver(post_save)
+def timestamped_modified(sender, update_fields, created, instance, **kwargs):
+    if isinstance(instance, Timestamped):
+        if instance.pk:
+            instance.modified = timezone.now()
